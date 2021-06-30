cad692252b381708fca3430d9cc36e1d29d883e1
sshahram <shirin.shahram23@gmail.com>
2021-06-30 13:46:06 -0700

    create typeDefs and resolvers files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,7 @@
+const { AuthenticationError } = require('apollo-server-express');
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,6 @@
+const { gql } = require('apollo-server-express');
+const typeDefs = gql`
+
+`;
+
+module.exports = typeDefs;
