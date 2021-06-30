e3dfec5a6a28748f8ba3998a07458a346419dba2
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-30 11:00:19 -0700

    profile layout from material ui is ready

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,7 +1,38 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+
 const Profile = () => {
+    const classes = useStyles();
     return (
         <div>
-            Hello world!
+            <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>Search bar</Paper>
+                </Grid>
+                <Grid item xs={3}>
+                    <Paper className={classes.paper}>Navigation</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={classes.paper}>Profile image</Paper>
+                </Grid>
+                <Grid item xs={5}>
+                    <Paper className={classes.paper}>Profile info</Paper>
+                </Grid>
+
+            </Grid>
         </div>
     )
 }
