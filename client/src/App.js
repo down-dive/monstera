faa83bd047289a55dd1db608dbdb55784aa91a35
Yevgeniya Terlyuk <43337986+down-dive@users.noreply.github.com>
2021-06-30 14:50:05 -0700

    Revert "Yev profile"

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-
-import Profile from './pages/Profile';
-import Friends from './pages/Friends';
-
 
 function App() {
   return (
-    <Router>
-    <div className="flex-column justify-flex-start min-100-vh">
-      <div className="container">
-        <Switch>
-          <Route exact path="/profile/:username?" component={Profile} />
-          <Route exact path="/friends" component={Friends} />
-        </Switch>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </Router>
   );
 }
 
