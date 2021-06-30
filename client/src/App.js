39b5a075cd1c5fd1ed19ec3a0d35143391c4b460
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-30 14:34:59 -0700

    removing logo

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ function App() {
   // </Router>
   <div className="App">
   <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
     <p>
       Edit <code>src/App.js</code> and save to reload.
     </p>
