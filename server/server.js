5e318409eea065a89eee6d70128c8006136f5f51
sshahram <shirin.shahram23@gmail.com>
2021-07-01 11:04:42 -0700

    resolve an issue with addUser mutation

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express = require('express');
+// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+// import authorization middlerware function
 const { authMiddleware } = require('./utils/auth');
-// configure our connection to the database
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// define server
+// create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
+// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
