031a7df03000b3810c61012c030375ebca4e5cc1
sshahram <shirin.shahram23@gmail.com>
2021-06-30 23:23:53 -0700

    add resolver to query single user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,20 @@
 const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user.username._id })
+                .select('-__v -password')
+                .populate('posts')
+                .populate('friends');
 
+            return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+    }
 };
 
 module.exports = resolvers;
