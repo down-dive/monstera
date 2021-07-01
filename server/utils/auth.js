a4e12093cadd25281a39cb4dd5bc320993cd6c3d
sshahram <shirin.shahram23@gmail.com>
2021-06-30 21:30:32 -0700

    define authMiddleware for authorization using JWT

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,5 +5,29 @@ require('dotenv').config();
 const secret = process.env.secret;
 const expiration = '2h';
 
-
+module.exports = {
+    authMiddleware: function ({ req }) {
+      // allows token to be sent via req.body, req.query, or headers
+      let token = req.body.token || req.query.token || req.headers.authorization;
+  
+      // ["Bearer", "<tokenvalue>"]
+      if (req.headers.authorization) {
+        token = token.split(' ').pop().trim();
+      }
+  
+      if (!token) {
+        return req;
+      }
+  
+      try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+      } catch {
+        console.log('Invalid token');
+      }
+  
+      return req;
+    },
+  };
+  
 
