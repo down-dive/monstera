02423c7a9ba7218efde2c244fcf2195fadd6e2cd
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-07-01 09:03:39 -0700

    added postform textarea

--- client/src/pages/homepage/index.js
+++ client/src/pages/homepage/index.js
@@ -4,6 +4,7 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
 import SearchBar from "../../components/Search-bar";
+import PostForm from "../../components/PostForm";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,22 +23,31 @@ const Homepage = () => {
     <div>
       <Grid container spacing={4}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>
-            Heading
-          </Paper>
+          <Paper className={classes.paper}>Heading</Paper>
         </Grid>
       </Grid>
-      <Grid container spacing={4}>
-        <Grid item xs={3}>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <Grid item xs={2}>
           <Paper className={classes.paper}>Navigation</Paper>
+          
         </Grid>
-        <Grid item xs={6}>
+        <Grid  item xs={6}>
           <Paper className={classes.paper}>
             <SearchBar />
           </Paper>
+          <Paper className={classes.paper}>
+              <PostForm />
+          </Paper>
+          <Paper className={classes.paper}>news feed</Paper>
         </Grid>
         <Grid item xs={3}>
-          <Paper className={classes.paper}>post form</Paper>
+        <Paper className={classes.paper}>local resources/links</Paper>
         </Grid>
       </Grid>
       <Grid
@@ -48,7 +58,7 @@ const Homepage = () => {
         spacing={4}
       >
         <Grid item xs={6}>
-          <Paper className={classes.paper}>news feed</Paper>
+          
         </Grid>
       </Grid>
     </div>
