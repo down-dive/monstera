f29628622013364e900d85443e801f23210fc09f
sshahram <shirin.shahram23@gmail.com>
2021-07-01 11:07:02 -0700

    setup pre-save middleware to create password

--- server/models/User.js
+++ server/models/User.js
@@ -49,10 +49,20 @@ const userSchema = new Schema(
     }
 );
 
-userSchema.methods.isCorrectPassword = async function (password) {
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // compare the incoming password with the hashed password
+  userSchema.methods.isCorrectPassword = async function(password) {
     return bcrypt.compare(password, this.password);
-};
-
+  };
 userSchema.virtual('friendCount').get(function () {
     return this.friends.length;
 });
