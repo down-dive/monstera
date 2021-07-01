2fbe6a70dfcecae031187d3be37fad4973666bbd
sshahram <shirin.shahram23@gmail.com>
2021-06-30 21:36:22 -0700

    define signToken for creating token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -28,6 +28,11 @@ module.exports = {
   
       return req;
     },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+    
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+      },
   };
   
 
