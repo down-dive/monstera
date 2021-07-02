6f1491e6fc878c79746a72fd31e1a8fe64cfdbdc
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-07-02 11:01:50 -0700

    added mutations and quaries on the client side and updated postForm to use mutation and quaries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,82 @@
+import gql from 'graphql-tag';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_POST = gql`
+  mutation addPost($postContent: String!) {
+    addPost(postContent: $postContent) {
+      _id
+      postContent
+      createdAt
+      username
+      replyCount
+      replies {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_REPLIES = gql`
+  mutation addReaction($postId: ID!, $replyBody: String!) {
+    addReaction(postId: $postId, replyBody: $replyBody) {
+      _id
+      replyCount
+      replies {
+        _id
+        replyBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_FRIEND = gql`
+  mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+      _id
+      username
+      friendCount
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_FRIEND = gql`
+  mutation removeFriend($id: ID!) {
+    removeFriend(id: $id) {
+      _id
+      username
+      friends {
+        _id
+        username
+      }
+    }
+  }
+`;
