fbf7f902ffffec5b5b89095763e89fa715324770
sshahram <shirin.shahram23@gmail.com>
2021-07-03 11:55:17 -0700

    create seeds for posts

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -35,6 +35,24 @@ db.once('open', async () => {
     await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
   }
 
+  // create posts
+  let createdPosts = [];
+  for (let i = 0; i < 100; i += 1) {
+    const postContent = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+
+    const createdPost = await Post.create({ postContent, username });
+
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $push: { posts: createdPost._id } }
+    );
+
+    createdPosts.push(createdPost);
+  }
+
   
   process.exit(0);
 });
