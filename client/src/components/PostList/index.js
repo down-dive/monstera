370ee74df7be74acb467ba23c0864bfaf05477b6
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-07-02 23:38:09 -0700

    removed paper from post list

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const PostList = ({ post, title }) => {
   // if (!post.length) {
@@ -11,29 +11,27 @@ const PostList = ({ post, title }) => {
       <h3>{title}</h3>
       {post &&
         post.map(post => (
-          <Paper className={classes.paper}>
-            <div key={post._id} className="card mb-3">
-              <p className="card-header">
-                <Link
-                  to={`/profile/${post.username}`}
-                  style={{ fontWeight: 700 }}
-                  className="text-light"
-                >
-                  {post.username}
-                </Link>{" "}
-                post on {post.createdAt}
-              </p>
-              <div className="card-body">
-                <Link to={`/post/${post._id}`}>
-                  <p>{post.postContent}</p>
-                  <p className="mb-0">
-                    Replies: {post.replyCount} || Click to{" "}
-                    {post.replyCount ? "see" : "start"} the discussion!
-                  </p>
-                </Link>
-              </div>
+          <div key={post._id} className="card mb-3">
+            <p className="card-header">
+              <Link
+                to={`/profile/${post.username}`}
+                style={{ fontWeight: 700 }}
+                className="text-light"
+              >
+                {post.username}
+              </Link>{" "}
+              post on {post.createdAt}
+            </p>
+            <div className="card-body">
+              <Link to={`/post/${post._id}`}>
+                <p>{post.postContent}</p>
+                <p className="mb-0">
+                  Replies: {post.replyCount} || Click to{" "}
+                  {post.replyCount ? "see" : "start"} the discussion!
+                </p>
+              </Link>
             </div>
-          </Paper>
+          </div>
         ))}
     </div>
   );
