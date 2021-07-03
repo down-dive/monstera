e011dfffb661972cd4229bde644f496c355ccf6e
sshahram <shirin.shahram23@gmail.com>
2021-07-03 11:54:34 -0700

    create seeds for users

---
+++ server/seeders/seeds.js
@@ -0,0 +1,40 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { User, Post } = require('../models');
+
+db.once('open', async () => {
+  await Post.deleteMany({});
+  await User.deleteMany({});
+
+  // create user data
+  const userData = [];
+
+  for (let i = 0; i < 50; i += 1) {
+    const username = faker.internet.userName();
+    const email = faker.internet.email(username);
+    const password = faker.internet.password();
+
+    userData.push({ username, email, password });
+  }
+
+  const createdUsers = await User.collection.insertMany(userData);
+
+  // create friends
+  for (let i = 0; i < 100; i += 1) {
+    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+    const { _id: userId } = createdUsers.ops[randomUserIndex];
+
+    let friendId = userId;
+
+    while (friendId === userId) {
+      const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+      friendId = createdUsers.ops[randomUserIndex];
+    }
+
+    await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
+  }
+
+  
+  process.exit(0);
+});
