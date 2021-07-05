744700a485f3b8d826f9c67723cf884fbf93e1d4
Kevin Lawrence <kevin.lawrence404@gmail.com>
2021-07-05 10:59:10 -0700

    updated profile page and friendslist page

--- client/src/pages/Friends/index.js
+++ client/src/pages/Friends/index.js
@@ -1,43 +1,55 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import FriendsList from '../../components/Friends-list'
-import SearchBar from '../../components/Search-bar'
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import FriendList from "../../components/Friends-list";
+import SearchBar from "../../components/Search-bar";
+import Auth from "../../utils/auth";
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_POSTS, QUERY_ME_BASIC } from "../../utils/queries";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
 }));
 
+const Profile = props => {
+  // const { loading, data } = useQuery(QUERY_POSTS);
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+  // const posts = data?.posts || [];
 
-const Profile = () => {
-    const classes = useStyles();
-    return (
-        <div>
-            <Grid container spacing={4}>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <SearchBar />
-                        </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>Navigation</Paper>
-                </Grid>
-                <Grid item xs={9}>
-                    <Paper className={classes.paper}>
-                        <FriendsList />
-                        </Paper>
-                </Grid>
-
-            </Grid>
-        </div>
-    )
-}
+  const loggedIn = Auth.loggedIn();
+  const classes = useStyles();
+  return (
+    <div>
+      <Grid container spacing={4}>
+        <Grid
+        //   container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        //   spacing={4}
+          item
+          xs={6}
+        >
+          {loggedIn && userData ? (
+            <Paper className={classes.paper}>
+              <FriendList
+                username={userData.me.username}
+                friendCount={userData.me.friendCount}
+                friends={userData.me.friends}
+              />
+            </Paper>
+          ) : null}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default Profile;
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,55 +1,85 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 
-import UserInfo from '../../components/User-info/index.js'
-import UserImage from '../../components/User-image'
-import SearchBar from '../../components/Search-bar'
-import UserPosts from  '../../components/User-posts'
+import PostForm from '../../components/PostForm';
+import PostList from '../../components/PostList';
+import FriendList from '../../components/Friends-list';
 
-//import './style.css'
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { QUERY_USER, QUERY_ME } from '../../utils/queries';
+import { ADD_FRIEND } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
+const Profile = props => {
+  const { username: userParam } = useParams();
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam }
+  });
 
-const Profile = () => {
-    const classes = useStyles();
+  const user = data?.me || data?.user || {};
+
+  // redirect to personal profile page if username is yours
+  if (
+    Auth.loggedIn() &&
+    Auth.getProfile().data.username === userParam
+  ) {
+    return <Redirect to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
     return (
-        <div>
-            <Grid container spacing={4}>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        <SearchBar />
-                        </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>Navigation</Paper>
-                </Grid>
-                <Grid item xs={4}>
-                    <Paper className={classes.paper}> User image
-                    <UserImage />
-                    </Paper>
-                </Grid>
-                <Grid item xs={5}>
-                    <Paper className={classes.paper}>
-                        <UserInfo />
-                        </Paper>
-                </Grid>
+      <h4>
+        You need to be logged in to see this. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
 
-            </Grid>
-            <UserPosts />
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+      </div>
+
+      <div className="flex-row justify-space-between mb-3">
+        <div className="col-12 mb-3 col-lg-8">
+          <PostList posts={user.posts} title={`${user.username}'s posts...`} />
         </div>
-    )
-}
 
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
+      </div>
+      <div className="mb-3">{!userParam && <PostForm />}</div>
+    </div>
+  );
+};
 
 export default Profile;
